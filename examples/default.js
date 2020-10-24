cef04177d0cf599633d5029bb09709c7d0d17bb3
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2020-10-24 17:42:32 +0530

    Completed Bulk for App

--- examples/default.js
+++ examples/default.js
@@ -32,7 +32,7 @@ function publisher() {
         ctr--
     };
     //console.time("Publishing");
-    Q.enque(payloads).then((result) => {
+    Q.tryEnque(payloads).then((result) => {
         //console.timeEnd("Publishing");
         publisherHandle = setTimeout(publisher, 1000);
     }).catch((err) => {
@@ -40,89 +40,90 @@ function publisher() {
     });
 }
 
-Q.deleteSubscriber().then(console.log).catch(console.error).then(()=>{
+// //Q.deleteSubscriber().then(console.log).catch(console.error).then(()=>{
 
-publisher();
-let reader = async () => {
-    let moreDataAvailable = false;
-    do {
-        const payload = await Q.tryDeque(10);
-        if (payload == null) {
-            moreDataAvailable = false;
-            console.log("No more data");
-        }
-        else {
-            let result = await Q.tryAcknowledge(payload.map(e => e.AckToken));
-            moreDataAvailable = result.retry === false && result.pending === null;
-            console.log(`Acked:${moreDataAvailable} Payload:${payload.length}`);
-        }
-    }
-    while (moreDataAvailable)
-};
-reader()
-    .then(console.log)
-    .catch(console.error)
-});
-
-// const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
-// const workers = [];
-// if (isMainThread) {
-//     let numberOfSubscribers = 10;
-//     const processingPerSub = 10;
-//     while (numberOfSubscribers > 0) {
-//         workers.push(new Promise((resolve, reject) => {
-//             const worker = new Worker(__filename, { workerData: [processingPerSub, ("Sub-" + numberOfSubscribers), 100] });
-//             worker.on('message', resolve);
-//             worker.on('error', reject);
-//             worker.on('exit', (code) => {
-//                 if (code !== 0)
-//                     reject(new Error(`Worker stopped with exit code ${code}`));
-//             });
-//         }));
-//         numberOfSubscribers--;
-//     }
-//     publisherHandle = setTimeout(publisher, 1000);
-//     Promise.all(workers).then((results) => {
-//         clearTimeout(publisherHandle);
-//         console.log(`${workers.length} Subscribers completed, Publisher Stopped.`);
-//         console.timeEnd("Application");
-//         pgp.end();
-//         console.table(results);
-//     }).catch(console.error);
-//     console.log(`1 Publisher(Main Thread) and ${workers.length} Subscribers active.`);
-//     console.time("Application");
-// }
-// else {
-//     const maxNumberofMessagesToFetch = workerData[0];
-//     const ThreadName = workerData[1];
-//     const sleepTime = workerData[2];
-//     const sleep = () => new Promise((a, r) => setTimeout(a, sleepTime));
-//     const waitForMessages = async (waitForMessageCount) => {
-//         let results = { "Subscriber": ThreadName, "Processed": [] }
-//         while (waitForMessageCount > 0) {
-//             //console.time("Deque-" + ThreadName);
-//             let payload = await Q.tryDeque();
-//             //console.timeEnd("Deque-" + ThreadName);
-//             if (payload != null) {
-//                 console.log(`Thread:${ThreadName} Acquired ${payload.Id.P}-${payload.Id.S}`);
-//                 let acked = false;
-//                 while (acked === false) {
-//                     //console.time("Ack-" + ThreadName);
-//                     acked = await Q.tryAcknowledge(payload.AckToken);
-//                     //console.timeEnd("Ack-" + ThreadName);
-//                     await sleep();
-//                 }
-//                 const logMsg = `Thread:${ThreadName} Acknowledged:${payload.Id.P}-${payload.Id.S} Payload: ${JSON.stringify(payload.Payload)}`;
-//                 console.log(logMsg);
-//                 await Q2.enque([{ "Log": logMsg }]);
-//                 results.Processed.push(payload);
-//                 waitForMessageCount--;
-//             }
-//             else {
-//                 await sleep();
-//             }
+// publisher();
+// let reader = async () => {
+//     let moreDataAvailable = false;
+//     do {
+//         const payload = await Q.tryDeque(10);
+//         if (payload == null) {
+//             moreDataAvailable = false;
+//             console.log("No more data");
 //         }
-//         return results;
-//     };
-//     waitForMessages(maxNumberofMessagesToFetch).then((r) => parentPort.postMessage(r));
-// }
+//         else {
+//             let result = await Q.tryAcknowledge(payload.map(e => e.AckToken));
+//             moreDataAvailable = result.retry === false && result.pending === null;
+//             console.log(`Acked:${moreDataAvailable} Payload:${payload.length}`);
+//         }
+//     }
+//     while (moreDataAvailable)
+// };
+// reader()
+//     .then(console.log)
+//     .catch(console.error)
+// //});
+
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+const workers = [];
+if (isMainThread) {
+    let numberOfSubscribers = 10;
+    const processingPerSub = 10;
+    while (numberOfSubscribers > 0) {
+        workers.push(new Promise((resolve, reject) => {
+            const worker = new Worker(__filename, { workerData: [processingPerSub, ("Sub-" + numberOfSubscribers), 100] });
+            worker.on('message', resolve);
+            worker.on('error', reject);
+            worker.on('exit', (code) => {
+                if (code !== 0)
+                    reject(new Error(`Worker stopped with exit code ${code}`));
+            });
+        }));
+        numberOfSubscribers--;
+    }
+    publisherHandle = setTimeout(publisher, 1000);
+    Promise.all(workers).then((results) => {
+        clearTimeout(publisherHandle);
+        console.log(`${workers.length} Subscribers completed, Publisher Stopped.`);
+        console.timeEnd("Application");
+        pgp.end();
+        console.table(results);
+    }).catch(console.error);
+    console.log(`1 Publisher(Main Thread) and ${workers.length} Subscribers active.`);
+    console.time("Application");
+}
+else {
+    const maxNumberofMessagesToFetch = workerData[0];
+    const ThreadName = workerData[1];
+    const sleepTime = workerData[2];
+    const sleep = () => new Promise((a, r) => setTimeout(a, sleepTime));
+    const waitForMessages = async (waitForMessageCount) => {
+        let results = { "Subscriber": ThreadName, "Processed": [] }
+        while (waitForMessageCount > 0) {
+            //console.time("Deque-" + ThreadName);
+            let payload = await Q.tryDeque();
+            //console.timeEnd("Deque-" + ThreadName);
+            if (payload != null) {
+                console.log(`Thread:${ThreadName} Acquired ${payload.map(e => (e.Id.P + '-' + e.Id.S)).join(',')}`);
+                let acked = false;
+                while (acked === false) {
+                    //console.time("Ack-" + ThreadName);
+                    acked = await Q.tryAcknowledge(payload.map(e => e.AckToken));
+                    acked = acked.retry === false && acked.pending === null;
+                    //console.timeEnd("Ack-" + ThreadName);
+                    await sleep();
+                }
+                const logMsg = `Thread:${ThreadName} Acknowledged:${payload.map(e => (e.Id.P + '-' + e.Id.S)).join(',')}`;
+                console.log(logMsg);
+                await Q2.tryEnque([{ "Log": logMsg }]);
+                results.Processed.push(payload);
+                waitForMessageCount--;
+            }
+            else {
+                await sleep();
+            }
+        }
+        return results;
+    };
+    waitForMessages(maxNumberofMessagesToFetch).then((r) => parentPort.postMessage(r));
+}
